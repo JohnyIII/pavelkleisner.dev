fe07416dc8463326561154ce62563142ad440ebd
Pavel Kleisner <pavel.kleisner@datalite.cz>
2026-01-02 14:07:28 +0100

    Initial Astro site

---
+++ src/i18n/cz.ts
@@ -0,0 +1,14 @@
+export default {
+    nav: {
+        home: "Domů",
+        about: "O mně",
+        projects: "Projekty",
+        contact: "Kontakt",
+    },
+    routes: {
+        home: "/cz",
+        about: "/cz/o-mne",
+        projects: "/cz/projekty",
+        contact: "/cz/kontakt",
+    },
+};
---
+++ src/i18n/en.ts
@@ -0,0 +1,14 @@
+export default {
+    nav: {
+        home: "Home",
+        about: "About",
+        projects: "Projects",
+        contact: "Contact",
+    },
+    routes: {
+        home: "/",
+        about: "/about",
+        projects: "/projects",
+        contact: "/contact",
+    },
+};
