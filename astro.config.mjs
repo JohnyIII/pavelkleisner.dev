537b868f21db784efd4a54dbeefd59131d1936e0
Pavel Kleisner <pavel.kleisner@datalite.cz>
2026-05-22 21:26:27 +0200

    uprava vzhledu a doplneni informaci, 404, sitemap, robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pavelkleisner.dev',
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          cz: 'cs-CZ',
+        },
+      },
+    }),
+  ],
   vite: {
     resolve: {
       alias: {
